f9ce6dcbc1d814ce8f25ccb7f79bd7e37086b961
Riens Winoto <rienslw@outlook.com>
2026-05-15 18:53:10 +0700

    fix: output instance profile name for ec2 instance profile attachment

--- terraform/modules/iam_role/outputs.tf
+++ terraform/modules/iam_role/outputs.tf
@@ -6,6 +6,6 @@ output "role_arn" {
   value = aws_iam_role.this.arn
 }
 
-output "instance_profile_arn" {
-  value = aws_iam_instance_profile.this.arn
+output "instance_profile_name" {
+  value = aws_iam_instance_profile.this.name
 }
